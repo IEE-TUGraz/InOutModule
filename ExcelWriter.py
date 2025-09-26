4cde4a5774e5cc8cbba05d165deecbcc345c6e6a
Felix C. A. Auer <10127354+FelixCAAuer@users.noreply.github.com>
2025-09-26 17:25:21 +0200

    Fix naming of write Power_VRES/-Profiles

--- ExcelWriter.py
+++ ExcelWriter.py
@@ -310,7 +310,7 @@ class ExcelWriter:
         """
         self._write_Excel_from_definition(dPower_ThermalGen, folder_path, "Power_ThermalGen")
 
-    def write_VRES(self, dPower_VRES: pd.DataFrame, folder_path: str) -> None:
+    def write_Power_VRES(self, dPower_VRES: pd.DataFrame, folder_path: str) -> None:
         """
         Write the dPower_VRES DataFrame to an Excel file in LEGO format.
         :param dPower_VRES: DataFrame containing the dPower_VRES data.
@@ -319,7 +319,7 @@ class ExcelWriter:
         """
         self._write_Excel_from_definition(dPower_VRES, folder_path, "Power_VRES")
 
-    def write_VRESProfiles(self, dPower_VRESProfiles: pd.DataFrame, folder_path: str) -> None:
+    def write_Power_VRESProfiles(self, dPower_VRESProfiles: pd.DataFrame, folder_path: str) -> None:
         """
         Write the dPower_VRESProfiles DataFrame to an Excel file in LEGO format.
         :param dPower_VRESProfiles: DataFrame containing the dPower_VRESProfiles data.
@@ -328,7 +328,7 @@ class ExcelWriter:
         """
         self._write_Excel_from_definition(dPower_VRESProfiles, folder_path, "Power_VRESProfiles")
 
-    def write_VRESProfiles_KInRows(self, dPower_VRESProfiles_KInRows: pd.DataFrame, folder_path: str) -> None:
+    def write_Power_VRESProfiles_KInRows(self, dPower_VRESProfiles_KInRows: pd.DataFrame, folder_path: str) -> None:
         """
         Write the dPower_VRESProfiles_KInRows DataFrame to an Excel file in LEGO format.
         :param dPower_VRESProfiles_KInRows: DataFrame containing the dPower_VRESProfiles_KInRows data.
@@ -459,9 +459,9 @@ if __name__ == "__main__":
         ("Power_Network", f"{args.caseStudyFolder}Power_Network.xlsx", ExcelReader.get_Power_Network, ew.write_Power_Network),
         ("Power_Storage", f"{args.caseStudyFolder}Power_Storage.xlsx", ExcelReader.get_Power_Storage, ew.write_Power_Storage),
         ("Power_ThermalGen", f"{args.caseStudyFolder}Power_ThermalGen.xlsx", ExcelReader.get_Power_ThermalGen, ew.write_Power_ThermalGen),
-        ("Power_VRES", f"{args.caseStudyFolder}Power_VRES.xlsx", ExcelReader.get_Power_VRES, ew.write_VRES),
-        ("Power_VRESProfiles", f"{args.caseStudyFolder}Power_VRESProfiles.xlsx", ExcelReader.get_Power_VRESProfiles, ew.write_VRESProfiles),
-        ("Power_VRESProfiles_KInRows", f"{args.caseStudyFolder}Power_VRESProfiles_KInRows.xlsx", ExcelReader.get_Power_VRESProfiles_KInRows, ew.write_VRESProfiles_KInRows),
+        ("Power_VRES", f"{args.caseStudyFolder}Power_VRES.xlsx", ExcelReader.get_Power_VRES, ew.write_Power_VRES),
+        ("Power_VRESProfiles", f"{args.caseStudyFolder}Power_VRESProfiles.xlsx", ExcelReader.get_Power_VRESProfiles, ew.write_Power_VRESProfiles),
+        ("Power_VRESProfiles_KInRows", f"{args.caseStudyFolder}Power_VRESProfiles_KInRows.xlsx", ExcelReader.get_Power_VRESProfiles_KInRows, ew.write_Power_VRESProfiles_KInRows),
         ("Power_WeightsK", f"{args.caseStudyFolder}Power_WeightsK.xlsx", ExcelReader.get_Power_WeightsK, ew.write_Power_WeightsK),
         ("Power_WeightsRP", f"{args.caseStudyFolder}Power_WeightsRP.xlsx", ExcelReader.get_Power_WeightsRP, ew.write_Power_WeightsRP),
         ("Power_Wind_TechnicalDetails", f"{args.caseStudyFolder}Power_Wind_TechnicalDetails.xlsx", ExcelReader.get_Power_Wind_TechnicalDetails, ew.write_Power_Wind_TechnicalDetails)
