6ac6c3153dc7bdc41523d433a4c6fcfed373bb83
Felix C. A. Auer <10127354+FelixCAAuer@users.noreply.github.com>
2025-10-30 16:14:05 +0100

    Print list of different files when using ExcelWriter to re-write CS

--- ExcelWriter.py
+++ ExcelWriter.py
@@ -545,6 +545,7 @@ if __name__ == "__main__":
         ("Power_Wind_TechnicalDetails", f"{args.caseStudyFolder}Power_Wind_TechnicalDetails.xlsx", ExcelReader.get_Power_Wind_TechnicalDetails, ew.write_Power_Wind_TechnicalDetails)
     ]
 
+    files_different = []
     for excel_definition_id, file_path, read, write in combinations:
         printer.information(f"Writing '{excel_definition_id}', read from '{file_path}'")
         if not os.path.exists(file_path):
@@ -563,5 +564,11 @@ if __name__ == "__main__":
                 printer.success(f"Excel files are equal")
             else:
                 printer.error(f"Excel files are NOT equal - see above for details")
+                files_different.append((excel_definition_id, file_path))
 
         printer.separator()
+    if len(files_different) > 0:
+        printer.error(f"Some Excel files were different:")
+        for excel_definition_id, file_path in files_different:
+            printer.error(f"'{excel_definition_id}' (file: '{file_path}')")
+        exit(1)
